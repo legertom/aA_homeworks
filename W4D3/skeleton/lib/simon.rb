d48eb6fe5f509fd5e7f5baac41327c4159d4867d
Tom Leger <tomleger@gmail.com>
2022-06-15 10:19:13 -0400

    completed simon rspec

--- W4D3/skeleton/lib/simon.rb
+++ W4D3/skeleton/lib/simon.rb
@@ -4,18 +4,34 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
 
   end
 
   def play
+    until @game_over == true
+      take_turn
+    end
+    game_over_message
+    reset_game
 
   end
 
   def take_turn
-
+    if @game_over == false
+      round_success_message
+      show_sequence
+      require_sequence
+      add_random_color
+      @sequence_length +=1
+    end
   end
 
   def show_sequence
+    add_random_color
+   
 
   end
 
@@ -24,6 +40,7 @@ class Simon
   end
 
   def add_random_color
+    seq << COLORS.sample
 
   end
 
@@ -32,10 +49,14 @@ class Simon
   end
 
   def game_over_message
+    puts "Game over"
+    reset_game
 
   end
 
   def reset_game
-
+    @game_over = false
+    @seq = []
+    @sequence_length = 1
   end
 end
